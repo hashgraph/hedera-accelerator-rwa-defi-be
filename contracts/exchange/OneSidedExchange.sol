17adb7a6585be1294792f88749a4d6fab50767cb
maryjanyes <maryjanyes@gmail.com>
2024-12-23 15:51:16 +0200

    feat (49507): complete 1sidedexchange contract
    Signed-off-by: maryjanyes <maryjanyes@gmail.com>

---
+++ contracts/exchange/OneSidedExchange.sol
@@ -0,0 +1,194 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+pragma abicoder v2;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {SafeTransferLib} from "../erc4626/SafeTransferLib.sol";
+
+struct Price {
+    uint256 price;
+    uint256 interval;
+}
+
+struct PriceThreshold {
+    uint256 maxSellAmount;
+    uint256 maxBuyAmount;
+    uint256 interval;
+}
+
+using SafeTransferLib for ERC20;
+
+/**
+ * @title One Sided Exchange
+ *
+ * The contract which represents a one sided exchange
+ at desired prices.
+ */
+contract OneSidedExchange is ReentrancyGuard, Ownable {
+    event Deposit(address token, uint256 amount);
+    event Withdraw(address token, uint256 amount);
+    event SwapSuccess(
+        address indexed trader,
+        address tokenA,
+        address tokenB,
+        uint256 tokenAAmount,
+        uint256 tokenBAmount
+    );
+    error InvalidAmount(string message, uint256 amount);
+    error NoPriceExists(string message);
+    error InvalidAddress(string message);
+
+    mapping(address => Price) internal _buyPrices;
+    mapping(address => Price) internal _sellPrices;
+    mapping(address => PriceThreshold) internal _thresholds;
+    mapping(address => uint256) internal _buyAmounts;
+    mapping(address => uint256) internal _sellAmounts;
+
+    constructor() Ownable(msg.sender) {}
+
+    modifier isValidAddress(address token) {
+        if (token == address(0)) {
+            revert InvalidAddress("No zero address is allowed");
+        }
+
+        _;
+    }
+
+    function withdraw(address token, uint256 amount) public nonReentrant onlyOwner isValidAddress(token) {
+        if (amount == 0) {
+            revert InvalidAmount("Invalid amount", amount);
+        }
+
+        if (ERC20(token).balanceOf(address(this)) < amount) {
+            revert InvalidAmount("Not enought tokens balance to withdraw", amount);
+        }
+
+        _withdraw(token, amount);
+    }
+
+    function deposit(address token, uint256 amount) public nonReentrant onlyOwner isValidAddress(token) {
+        if (amount == 0) {
+            revert InvalidAmount("Invalid amount", amount);
+        }
+
+        _deposit(msg.sender, token, amount);
+    }
+
+    function swap(address tokenA, address tokenB, uint256 amount) public nonReentrant {
+        _swap(msg.sender, tokenA, tokenB, amount);
+    }
+
+    function _deposit(address signer, address token, uint256 amount) internal {
+        /// @notice Owner should give allowance for a contract to transfer n tokens amount.
+        uint256 _amount = amount * (10 ** ERC20(token).decimals());
+        ERC20(token).transferFrom(signer, address(this), _amount);
+
+        emit Deposit(token, amount);
+    }
+
+    function _withdraw(address token, uint256 amount) internal {
+        uint256 _amount = amount * (10 ** ERC20(token).decimals());
+        ERC20(token).transfer(owner(), _amount);
+
+        emit Withdraw(token, amount);
+    }
+
+    function _swap(address trader, address tokenA, address tokenB, uint256 amount) internal {
+        (uint256 tokenAAmount, uint256 tokenBAmount) = _checkIfExchangeAllowedForPair(tokenA, tokenB, amount, trader);
+
+        /// @notice Owner should give allowance for a contract to transfer n tokens amount.
+        ERC20(tokenA).transferFrom(trader, address(this), tokenAAmount);
+        ERC20(tokenB).transfer(trader, tokenBAmount);
+
+        _sellAmounts[tokenA] += tokenAAmount;
+        _buyAmounts[tokenB] += tokenBAmount;
+
+        emit SwapSuccess(trader, tokenA, tokenB, tokenAAmount, tokenBAmount);
+    }
+
+    function _checkIfExchangeAllowedForPair(
+        address tokenA,
+        address tokenB,
+        uint256 amount,
+        address trader
+    ) internal view returns (uint256 tokenAAmount, uint256 tokenBAmount) {
+        PriceThreshold memory tokenAThreshold = _thresholds[tokenA];
+        PriceThreshold memory tokenBThreshold = _thresholds[tokenB];
+
+        (uint256 tokenASellAmount, uint256 tokenBBuyAmount) = estimateTokenReturns(tokenA, tokenB, amount);
+        uint256 balanceOfAToken = ERC20(tokenA).balanceOf(trader) * (10 ** ERC20(tokenA).decimals());
+        uint256 balanceOfBToken = ERC20(tokenB).balanceOf(address(this)) * (10 ** ERC20(tokenB).decimals());
+
+        if (tokenASellAmount > balanceOfAToken) {
+            revert InvalidAmount("No enough tokens to sell", tokenASellAmount);
+        } else if (tokenBBuyAmount > balanceOfBToken) {
+            revert InvalidAmount("No enough tokens to buy", tokenBBuyAmount);
+        }
+
+        if (tokenAThreshold.maxSellAmount != 0 && tokenAThreshold.interval < block.timestamp) {
+            if ((_sellAmounts[tokenA] + tokenASellAmount) > tokenAThreshold.maxSellAmount) {
+                revert InvalidAmount("Max sell amount of tokens exceeded", (_sellAmounts[tokenA] + tokenASellAmount));
+            }
+        }
+
+        if (tokenBThreshold.maxBuyAmount != 0 && tokenBThreshold.interval < block.timestamp) {
+            if ((_buyAmounts[tokenB] + tokenBBuyAmount) > tokenBThreshold.maxBuyAmount) {
+                revert InvalidAmount("Max buy amount of tokens exceeded", (_buyAmounts[tokenB] + tokenBBuyAmount));
+            }
+        }
+
+        return (tokenASellAmount, tokenBBuyAmount);
+    }
+
+    function estimateTokenReturns(
+        address tokenA,
+        address tokenB,
+        uint256 amount
+    ) public view isValidAddress(tokenA) isValidAddress(tokenB) returns (uint256 tokenAAmount, uint256 tokenBAmount) {
+        if (_buyPrices[tokenB].interval == 0 || _sellPrices[tokenA].interval == 0) {
+            revert NoPriceExists("Sell or buy price for pair not found");
+        } else if (_buyPrices[tokenB].interval > block.timestamp || _sellPrices[tokenA].interval > block.timestamp) {
+            revert NoPriceExists("Sell or buy price for pair is not valid");
+        }
+
+        uint256 tokenADecimals = 10 ** ERC20(tokenA).decimals();
+        uint256 tokenBDecimals = 10 ** ERC20(tokenB).decimals();
+
+        if (tokenADecimals != tokenBDecimals) {
+            uint256 tokenASellAmount = amount * _sellPrices[tokenA].price;
+            uint256 tokenBBuyAmount = ((tokenASellAmount * _sellPrices[tokenA].price) / _buyPrices[tokenB].price);
+
+            return (tokenASellAmount * tokenADecimals, tokenBBuyAmount * tokenBDecimals);
+        } else {
+            uint256 tokenASellAmount = (amount * tokenADecimals) * _sellPrices[tokenA].price;
+            uint256 tokenBBuyAmount = ((tokenASellAmount * _sellPrices[tokenA].price) / _buyPrices[tokenB].price);
+
+            return (tokenASellAmount, tokenBBuyAmount);
+        }
+    }
+
+    function setThreshold(
+        address token,
+        uint256 maxSellAmount,
+        uint256 maxBuyAmount,
+        uint256 interval
+    ) public onlyOwner isValidAddress(token) {
+        uint256 _decimals = ERC20(token).decimals();
+
+        _thresholds[token] = PriceThreshold(
+            maxSellAmount * (10 ** _decimals),
+            maxBuyAmount * (10 ** _decimals),
+            interval
+        );
+    }
+
+    function setBuyPrice(address token, uint256 amount, uint256 interval) public onlyOwner isValidAddress(token) {
+        _buyPrices[token] = Price(amount, interval);
+    }
+
+    function setSellPrice(address token, uint256 amount, uint256 interval) public onlyOwner isValidAddress(token) {
+        _sellPrices[token] = Price(amount, interval);
+    }
+}
---
+++ test/exchange/one-sided-exchange.test.js
@@ -0,0 +1,109 @@
+import { expect } from "chai";
+
+async function preDeployExchange() {
+    const [deployer] = await ethers.getSigners();
+    const oneSidedExchangeImplementation = await ethers.deployContract("OneSidedExchange", deployer);
+    const tokenAImplementation = await ethers.deployContract("ExampleERC20", [100, "TokenA", "TOKA"], deployer);
+    const tokenBImplementation = await ethers.deployContract("ExampleERC20", [100, "TokenB", "TOKB"], deployer);
+
+    return [oneSidedExchangeImplementation, tokenAImplementation, tokenBImplementation];
+}
+
+describe("OneSidedExchange", async () => {
+    let oneSidedExchangeInstance;
+    let tokenAInstance;
+    let tokenBInstance;
+    let owner;
+
+    before(async () => {
+        const [deployer] = await ethers.getSigners();
+        [oneSidedExchangeInstance, tokenAInstance, tokenBInstance] = await preDeployExchange();
+        owner = deployer;
+    })
+
+    it("Should successfuly swap tokenA and tokenB", async () => {
+        const exchangeAddress = await oneSidedExchangeInstance.getAddress();
+        const tokenAAddress = await tokenAInstance.getAddress();
+        const tokenADecimals = await tokenAInstance.decimals();
+        const tokenBAddress = await tokenBInstance.getAddress();
+        const tokenBDecimals = await tokenBInstance.decimals();
+        // Set days threshold to 2.
+        const twoDaysAfter = new Date().getSeconds() + (((24 * 60) * 60) * 2);
+        // Set token swap amount to 2.
+        const tokenASwapAmount = 2n;
+
+        // Set sell price for token A to 6.
+        await oneSidedExchangeInstance.setSellPrice(tokenAAddress, 6, twoDaysAfter);
+        // Set buy price for token B to 4.
+        await oneSidedExchangeInstance.setBuyPrice(tokenBAddress, 4, twoDaysAfter);
+        // Approve exchange to use n amount of tokens.
+        await tokenAInstance.approve(exchangeAddress, (100n * (10n ** tokenADecimals)));
+        await tokenBInstance.approve(exchangeAddress, (100n * (10n ** tokenBDecimals)));
+
+        const [_, buyAmount] = (await oneSidedExchangeInstance.estimateTokenReturns(tokenAAddress, tokenBAddress, tokenASwapAmount));
+        const exchangeTokenABalanceBeforeSwap = await tokenAInstance.balanceOf(exchangeAddress);
+        const swapperTokenBBalanceBeforeSwap = await tokenBInstance.balanceOf(owner);
+
+        await oneSidedExchangeInstance.deposit(tokenBAddress, (buyAmount / (10n ** tokenBDecimals)) + 2n);
+        await oneSidedExchangeInstance.swap(tokenAAddress, tokenBAddress, tokenASwapAmount);
+
+        const exchangeTokenABalanceAfterSwap = await tokenAInstance.balanceOf(exchangeAddress);
+        const swapperTokenBBalanceAfterSwap = await tokenBInstance.balanceOf(owner);
+        const exchangeABalanceBefore = exchangeTokenABalanceBeforeSwap / (10n ** tokenADecimals);
+        const exchangeABalanceAfter = exchangeTokenABalanceAfterSwap / (10n ** tokenADecimals);
+        const swapperBBalanceBefore = swapperTokenBBalanceBeforeSwap / (10n ** tokenBDecimals);
+        const swapperBBalanceAfter = swapperTokenBBalanceAfterSwap / (10n ** tokenBDecimals);
+
+        expect(exchangeABalanceBefore).to.be.equal(0n);
+        expect(exchangeABalanceAfter).to.be.equal(12n);
+        expect(swapperBBalanceBefore).to.be.equal(100n);
+        expect(swapperBBalanceAfter).to.be.equal(98n);
+    });
+
+    it("Should fail on swap tokenA and tokenB", async () => {
+        const exchangeAddress = await oneSidedExchangeInstance.getAddress();
+        const tokenAAddress = await tokenAInstance.getAddress();
+        const tokenADecimals = await tokenAInstance.decimals();
+        const tokenBAddress = await tokenBInstance.getAddress();
+        const tokenBDecimals = await tokenBInstance.decimals();
+
+        // Set days threshold to 2.
+        const twoDaysAfter = new Date().getSeconds() + (((24 * 60) * 60) * 2);
+        // Set token swap amount to 2.
+        const tokenASwapAmount = 2n;
+        // Set token A max sell amount to 16 and max buy amount 10.
+        const tokenASellAmount = 16n;
+        const tokenABuyAmount = 10n;
+
+        // Set sell price for token A to 6.
+        await oneSidedExchangeInstance.setSellPrice(tokenAAddress, 6, twoDaysAfter);
+        // Set buy price for token B to 4.
+        await oneSidedExchangeInstance.setBuyPrice(tokenBAddress, 4, twoDaysAfter);
+        await oneSidedExchangeInstance.setThreshold(tokenAAddress, tokenASellAmount, tokenABuyAmount, twoDaysAfter);
+        await tokenAInstance.approve(exchangeAddress, (100n * (10n ** tokenADecimals)));
+        await tokenBInstance.approve(exchangeAddress, (100n * (10n ** tokenBDecimals)));
+
+        const exchangeTokenABalanceBeforeSwap = await tokenAInstance.balanceOf(exchangeAddress);
+        const swapperTokenBBalanceBeforeSwap = await tokenBInstance.balanceOf(owner);
+
+        try {
+            await oneSidedExchangeInstance.swap(tokenAAddress, tokenBAddress, tokenASwapAmount);
+
+            const exchangeTokenABalanceAfterSwap = await tokenAInstance.balanceOf(exchangeAddress);
+            const swapperTokenBBalanceAfterSwap = await tokenBInstance.balanceOf(owner);
+            const exchangeABalanceBefore = exchangeTokenABalanceBeforeSwap / (10n ** tokenADecimals);
+            const exchangeABalanceAfter = exchangeTokenABalanceAfterSwap / (10n ** tokenADecimals);
+            const swapperBBalanceBefore = swapperTokenBBalanceBeforeSwap / (10n ** tokenBDecimals);
+            const swapperBBalanceAfter = swapperTokenBBalanceAfterSwap / (10n ** tokenBDecimals);
+
+            expect(exchangeABalanceBefore).to.be.equal(0n);
+            expect(exchangeABalanceAfter).to.be.equal(12n);
+            expect(swapperBBalanceBefore).to.be.equal(100n);
+            expect(swapperBBalanceAfter).to.be.equal(98n);
+        } catch (err) {
+            const parsedMessage = err.message?.split("InvalidAmount")[1];
+
+            expect(parsedMessage).to.be.includes("Max sell amount of tokens exceeded");
+        }
+    });
+})
