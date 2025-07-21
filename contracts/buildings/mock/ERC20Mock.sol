9a8c09361c98c01472ebe7d68a49fc91d663d2bd
Bruno Campos <bhncampos@gmail.com>
2025-07-21 01:05:55 -0300

    trade with permit
    Signed-off-by: Bruno Campos <bhncampos@gmail.com>

--- contracts/buildings/mock/ERC20Mock.sol
+++ contracts/buildings/mock/ERC20Mock.sol
@@ -2,15 +2,16 @@
 pragma solidity ^0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit, Nonces} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
-contract ERC20Mock is ERC20, ERC20Votes {
+contract ERC20Mock is ERC20, ERC20Votes, ERC20Permit {
     uint8 private customDecimals;
 
     constructor(string memory _name, string memory _symbol, uint8 _decimals)
         ERC20(_name, _symbol)
-        EIP712(_name, "1") // Must explicitly call EIP712 constructor
+        ERC20Permit(_name) // Must explicitly call ERC20Permit constructor
     {
         customDecimals = _decimals;
     }
@@ -38,6 +39,10 @@ contract ERC20Mock is ERC20, ERC20Votes {
     // solhint-disable-next-line func-name-mixedcase
     function CLOCK_MODE() public pure override returns (string memory) {
         return "mode=timestamp";
+    }
+
+    function nonces(address owner) public view override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
     }
 
 }
