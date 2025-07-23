9a7193b09ae4d854943561772a3e3e1aefe3b24f
Bruno Campos <bhncampos@gmail.com>
2025-07-22 23:42:41 -0300

    redeploy usdc
    Signed-off-by: Bruno Campos <bhncampos@gmail.com>

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
 export const uniswapRouterAddress = "0x3322f84A829Ed1D675DDa0df97f041A6463a921D";
 export const uniswapFactoryAddress = "0x679261029c4e9B704bB54F6f4AF5241080191377";
 export const chainlinkAggregatorMockAddress = "0x269501f5674BeE3E8fef90669d3faa17021344d0";
-export const usdcAddress = "0x454F9FfACBD6452857a43551ea50495c724b1CeA";
+export const usdcAddress = "0x80304c8473A203c175A58eB6C1Fd0A825C3c9a78";
